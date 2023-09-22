753e3e5f53507a9be47047857903af12ec78c6f7
oereo <dlstpgns0406@gmail.com>
2023-09-22 16:59:19 +0900

    chore: project basic setting

---
+++ src/main/java/com/github/slacksentry/SlackSentryApplication.java
@@ -0,0 +1,13 @@
+package com.github.slacksentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackSentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackSentryApplication.class, args);
+	}
+
+}
