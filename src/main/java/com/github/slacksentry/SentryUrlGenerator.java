ccbe0b2b6bace5e511d2e270f75088aff4a481a0
oereo <dlstpgns0406@gmail.com>
2023-09-27 22:04:56 +0900

    feat: sentry domain pattern 매칭 로직 추가

--- src/main/java/com/github/slacksentry/SentryUrlGenerator.java
+++ src/main/java/com/github/slacksentry/SentryUrlGenerator.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class SentryUrlGenerator {
     private static final String PERFORMANCE_PATH = "performance";
     private static final String HTTP_PATTERN = "^(http|https)://";
+    private static final String SENTRY_DOMAIN_PATTERN = "sentry\\.io";
 
     private static final String SLASH = "/";
     private static final String COLON = ":";
@@ -27,11 +28,19 @@ public class SentryUrlGenerator {
             throw new IllegalArgumentException("sentryBaseUrl cannot be null or empty");
         }
 
-        Pattern pattern = Pattern.compile(HTTP_PATTERN);
-        Matcher matcher = pattern.matcher(sentryBaseUrl);
+        Pattern httpPattern = Pattern.compile(HTTP_PATTERN);
+        Matcher httpMatcher = httpPattern.matcher(sentryBaseUrl);
 
-        if (!matcher.find()) {
+        if (!httpMatcher.find()) {
             throw new IllegalArgumentException("Invalid protocol in sentryBaseUrl");
         }
+
+        Pattern sentryDomainPattern = Pattern.compile(SENTRY_DOMAIN_PATTERN);
+        Matcher sentryDomainMatcher = sentryDomainPattern.matcher(sentryBaseUrl);
+
+        if (!sentryDomainMatcher.find()) {
+            throw new IllegalArgumentException("sentryBaseUrl must contain 'sentry.io'");
+        }
+
     }
 }
