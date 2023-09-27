1ebce53b6c21d9a267e07c791afef5a35daaa248
oereo <dlstpgns0406@gmail.com>
2023-09-27 18:07:50 +0900

    feat: sentryBaseUrl validate method 구현

--- src/main/java/com/github/slacksentry/SentryUrlGenerator.java
+++ src/main/java/com/github/slacksentry/SentryUrlGenerator.java
@@ -1,23 +1,37 @@
 package com.github.slacksentry;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SentryUrlGenerator {
-    private static final String URL_PROTOCOL = "https";
-    private static final String HOST = "sentry.io";
     private static final String PERFORMANCE_PATH = "performance";
+    private static final String HTTP_PATTERN = "^(http|https)://";
+
     private static final String SLASH = "/";
     private static final String COLON = ":";
 
     private SentryUrlGenerator() {};
 
-    public static String getSentryPerformanceUrl(String sentryAccountName, String sentryProjectName) {
-        return URL_PROTOCOL +
-                COLON + SLASH + SLASH +
-                sentryAccountName +
-                HOST +
+    public static String getSentryPerformanceUrl(String sentryBaseUrl, String sentryProjectName) {
+        validateSentryBaseUrl(sentryBaseUrl);
+        return sentryBaseUrl +
                 SLASH +
                 PERFORMANCE_PATH +
                 SLASH +
                 sentryProjectName +
                 COLON;
+    }
+
+    private static void validateSentryBaseUrl(String sentryBaseUrl) {
+        if (sentryBaseUrl == null || sentryBaseUrl.isEmpty()) {
+            throw new IllegalArgumentException("sentryBaseUrl cannot be null or empty");
+        }
+
+        Pattern pattern = Pattern.compile(HTTP_PATTERN);
+        Matcher matcher = pattern.matcher(sentryBaseUrl);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Invalid protocol in sentryBaseUrl");
+        }
     }
 }
