76153ec7da9808dd8cb2ce532ba0498e2a9b633e
oereo <dlstpgns0406@gmail.com>
2023-09-30 09:29:24 +0900

    refactor: sentryBaseUrl에 대한 validation logic을 SlackSentryCredentials 객체 생성시에 검증할 수 있도록 변경

--- src/main/java/com/github/slacksentry/SentryUrlGenerator.java
+++ src/main/java/com/github/slacksentry/SentryUrlGenerator.java
@@ -1,12 +1,7 @@
 package com.github.slacksentry;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class SentryUrlGenerator {
     private static final String PERFORMANCE_PATH = "performance";
-    private static final String HTTP_PATTERN = "^(http|https)://";
-    private static final String SENTRY_DOMAIN_PATTERN = "sentry\\.io";
 
     private static final String SLASH = "/";
     private static final String COLON = ":";
@@ -14,33 +9,11 @@ public class SentryUrlGenerator {
     private SentryUrlGenerator() {};
 
     public static String getSentryPerformanceUrl(String sentryBaseUrl, String sentryProjectName) {
-        validateSentryBaseUrl(sentryBaseUrl);
         return sentryBaseUrl +
                 SLASH +
                 PERFORMANCE_PATH +
                 SLASH +
                 sentryProjectName +
                 COLON;
-    }
-
-    private static void validateSentryBaseUrl(String sentryBaseUrl) {
-        if (sentryBaseUrl == null || sentryBaseUrl.isEmpty()) {
-            throw new IllegalArgumentException("sentryBaseUrl cannot be null or empty");
-        }
-
-        Pattern httpPattern = Pattern.compile(HTTP_PATTERN);
-        Matcher httpMatcher = httpPattern.matcher(sentryBaseUrl);
-
-        if (!httpMatcher.find()) {
-            throw new IllegalArgumentException("Invalid protocol in sentryBaseUrl");
-        }
-
-        Pattern sentryDomainPattern = Pattern.compile(SENTRY_DOMAIN_PATTERN);
-        Matcher sentryDomainMatcher = sentryDomainPattern.matcher(sentryBaseUrl);
-
-        if (!sentryDomainMatcher.find()) {
-            throw new IllegalArgumentException("sentryBaseUrl must contain 'sentry.io'");
-        }
-
     }
 }
--- src/main/java/com/github/slacksentry/SlackSentryCredentials.java
+++ src/main/java/com/github/slacksentry/SlackSentryCredentials.java
@@ -1,16 +1,53 @@
 package com.github.slacksentry;
 
-public class SlackSentryCredentials {
-    private final String webhookUrl;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public SlackSentryCredentials(String webhookUrl) {
-        if (webhookUrl == null) {
-            throw new IllegalArgumentException("webhookUrl cannot be null.");
-        }
+public class SlackSentryCredentials {
+    private static final String HTTP_PATTERN = "^(http|https)://";
+    private static final String SENTRY_DOMAIN_PATTERN = "sentry\\.io";
+    private final String webhookUrl;
+    private final String sentryBaseUrl;
+
+    public SlackSentryCredentials(String webhookUrl, String sentryBaseUrl) {
+        validateSlackWebhookUrl(webhookUrl);
+        validateSentryBaseUrl(sentryBaseUrl);
+
         this.webhookUrl = webhookUrl;
+        this.sentryBaseUrl = sentryBaseUrl;
     }
 
     public String getWebhookUrl() {
         return webhookUrl;
+    }
+
+    public String getSentryBaseUrl() {
+        return sentryBaseUrl;
+    }
+
+    private void validateSlackWebhookUrl(String webhookUrl) {
+        if (webhookUrl == null || webhookUrl.isEmpty()) {
+            throw new IllegalArgumentException("webhookUrl cannot be null or empty.");
+        }
+    }
+
+    private void validateSentryBaseUrl(String sentryBaseUrl) {
+        if (sentryBaseUrl == null || sentryBaseUrl.isEmpty()) {
+            throw new IllegalArgumentException("sentryBaseUrl cannot be null or empty.");
+        }
+
+        Pattern httpPattern = Pattern.compile(HTTP_PATTERN);
+        Matcher httpMatcher = httpPattern.matcher(sentryBaseUrl);
+
+        if (!httpMatcher.find()) {
+            throw new IllegalArgumentException("Invalid protocol in sentryBaseUrl.");
+        }
+
+        Pattern sentryDomainPattern = Pattern.compile(SENTRY_DOMAIN_PATTERN);
+        Matcher sentryDomainMatcher = sentryDomainPattern.matcher(sentryBaseUrl);
+
+        if (!sentryDomainMatcher.find()) {
+            throw new IllegalArgumentException("sentryBaseUrl must contain 'sentry.io'");
+        }
     }
 }
